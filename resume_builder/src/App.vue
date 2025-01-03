9410bf42e8a880b1a31214c719b11529d544f1c1
Rachid Boukind <boukind.r1@gmail.com>
2025-01-03 23:23:18 +0100

    preparing component state

--- resume_builder/src/App.vue
+++ resume_builder/src/App.vue
@@ -2,52 +2,63 @@
   <main class="container">
       <div id="resume" class="d-flex">
         <div class="left-col">
-          <h4 class="section-headline">
-            About me
-          </h4>
-          <div>
-            I am a 10x software engieer
+          <div class="resume-section">
+
+            <img :src="imageUrl" class="profile-pic" alt="profile picture">
+
+            <h4 class="section-headline">
+              {{ headlines[0] }}
+            </h4>
+            <div>
+              {{ introText }}
+            </div>
           </div>
-          <h4 class="section-headline">
-            Contact
-          </h4>
-          <ul>
-            <li>+2126959465</li>
-            <li>contact@gmail.com</li>
-            <li>Morocco-agadir</li>
-          </ul>
-          <h4 class="section-headline">
-            Skills
-          </h4>
-          <ul>
-            <li>Python</li>
-            <li>Pandas</li>
-            <li>SQL</li>
-            <li>R</li>
-            <li>Machine Learning</li>
-          </ul>
-          <h4 class="section-headline">
-            Certifications
-          </h4>
-          <ul>
-            <li>Natural Language Processing with Python (Coursera)</li>
-            <li>Recommendation Systems with TensorFlow on GCP (Google)</li>
-          </ul>
+          <div class="resume-section">
+            <h4 class="section-headline">
+              {{ headlines[1] }}
+            </h4>
+            <ul>
+              <li>{{ contact.phone }}</li>
+              <li>{{ contact.email }}</li>
+              <li>{{ contact.address }}</li>
+            </ul>
+          </div>
+          <div class="resume-section">
+            <h4 class="section-headline">
+              {{ headlines[2] }}
+            </h4>
+            <ul>
+              <li>Python</li>
+              <li>Pandas</li>
+              <li>SQL</li>
+              <li>R</li>
+              <li>Machine Learning</li>
+            </ul>
+          </div>
+          <div class="resume-section">
+            <h4 class="section-headline">
+              {{ headlines[3] }}
+            </h4>
+            <ul>
+              <li>Natural Language Processing with Python (Coursera)</li>
+              <li>Recommendation Systems with TensorFlow on GCP (Google)</li>
+            </ul>
+          </div>
         </div>
         <div class="right-col">
           <div class="personal-name">
-            Jhon stones
+            {{ name }}
           </div>
           <div class="personal-title">
-            Software developer
+            {{ title }}
           </div>
           <h4 class="section-headline">
-            Experience
+            {{ headlines[4] }}
           </h4>
           <div>Senior software engieer</div>
           <div>Blackrocket Inc., London, 2023 - Present</div>
           <h4 class="section-headline">
-            Education
+            {{ headlines[5] }}
           </h4>
           <div>Bachelor of Science in Computer Science</div>
           <div>Evergreen State University, Springdale, 2018-2022</div>
@@ -59,6 +70,26 @@
       </div>
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Jhone Stones",
+      title: "Software engieer",
+      introText: "I am a 10x software engieer.",
+      imageUrl: "/profile_pic.jpg",
+      headlines: ["About me", "Contact", "Skills", "Certifications", "Experience", "Education"],
+      contact: {
+        phone: "+21268498798",
+        email: "contact@gmail.com",
+        address: "nice place here"
+      },
+      skills: ["Python", "Pandas", "SQL", "R", "AI", "C++", "Machine Learning", "Hadoop", "TensorFlow", "PyTorch", "NLP"],
+    }
+  }
+}
+</script>
 
 <style scoped>
   #resume {
@@ -127,5 +158,17 @@
     padding-inline-start: 16px;
     margin-block-end: 0px;
     margin-block-start: 5px;
+  }
+
+  .profile-pic {
+    display: block;
+    width: 160px;
+    height: 160px;
+    border: 5px solid var(--highlight-color-left);
+    margin-bottom: 20px;
+    object-fit: cover;
+    margin-left: auto;
+    margin-right: auto;
+    border-radius: 50%;
   }
 </style>
